df8e1b6eba79a66dc5af838f23ba8aab18328e62
Alex J Lennon <ajlennon@dynamicdevices.co.uk>
2021-10-26 15:23:05 +0100

    cheerlight_spotify.sh: Add wrapper script for envvars
    Signed-off-by: Alex J Lennon <ajlennon@dynamicdevices.co.uk>

---
+++ cheerlights_spotify.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+# ENTER THE DETAILS HERE FROM THE WEB APP YOU CREATE - SEE README.md FOR DETAILS
+export SPOTIPY_CLIENT_ID=
+export SPOTIPY_CLIENT_SECRET=
+export SPOTIPY_REDIRECT_URI=http://127.0.0.1/dummy
+
+# Run the Python script
+./cheerlights_spotify.py
